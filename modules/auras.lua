eef0a10e19101c9c86c0102a55fa735cd25b0e4f
slaren <ddevesa@gmail.com>
2013-04-25 13:51:41 +0200

    auras: cleanup

--- modules/auras.lua
+++ modules/auras.lua
@@ -1006,101 +1006,95 @@ end
 
 function Auras:GetAuraList()
 	local auraTable = setmetatable({
-		-- Spell Name			Priority (higher = more priority)
+		-- Spell NamePriority (higher = more priority)
 		-- Crowd control
-		[GetSpellInfo(33786)] 	= 3, 	-- Cyclone
-		[GetSpellInfo(2637)] 	= 3,	-- Hibernate
-		[GetSpellInfo(55041)] 	= 3, 	-- Freezing Trap Effect
-		[GetSpellInfo(3355)]	 = 3, -- Freezing Trap (from trap launcher)
-		[GetSpellInfo(6770)]		= 3, 	-- Sap
-		[GetSpellInfo(2094)]		= 3, 	-- Blind
-		[GetSpellInfo(5782)]		= 3, 	-- Fear
-		[GetSpellInfo(6789)]		= 3,	-- Death Coil Warlock
-		[GetSpellInfo(64044)]	= 3, -- Psychic Horror
-		[GetSpellInfo(6358)]	 = 3, 	-- Seduction
-		[GetSpellInfo(5484)]	 = 3, 	-- Howl of Terror
-		[GetSpellInfo(5246)]	 = 3, 	-- Intimidating Shout
-		[GetSpellInfo(8122)]	 = 3,	-- Psychic Scream
-		[GetSpellInfo(118)]	  = 3,	-- Polymorph
-		[GetSpellInfo(28272)] 	= 3,	-- Polymorph pig
-		[GetSpellInfo(28271)] 	= 3,	-- Polymorph turtle
-		[GetSpellInfo(61305)] 	= 3,	-- Polymorph black cat
-		[GetSpellInfo(61025)] 	= 3,	-- Polymorph serpent
-		[GetSpellInfo(51514)]	= 3,	-- Hex
-		[GetSpellInfo(710)]		= 3,	-- Banish
-		[GetSpellInfo(1499)]	 = 3, -- Freezing Trap Effect
-		[GetSpellInfo(60192)]	= 3, -- Freezing Trap (from trap launcher)
+		[GetSpellInfo(33786)] = 3, -- Cyclone
+		[GetSpellInfo(2637)] = 3, -- Hibernate
+		[GetSpellInfo(55041)] = 3, -- Freezing Trap Effect
+		[GetSpellInfo(3355)] = 3, -- Freezing Trap (from trap launcher)
+		[GetSpellInfo(6770)] = 3, -- Sap
+		[GetSpellInfo(2094)] = 3, -- Blind
+		[GetSpellInfo(5782)] = 3, -- Fear
+		[GetSpellInfo(6789)] = 3, -- Death Coil Warlock
+		[GetSpellInfo(64044)] = 3, -- Psychic Horror
+		[GetSpellInfo(6358)] = 3, -- Seduction
+		[GetSpellInfo(5484)] = 3, -- Howl of Terror
+		[GetSpellInfo(5246)] = 3, -- Intimidating Shout
+		[GetSpellInfo(8122)] = 3, -- Psychic Scream
+		[GetSpellInfo(118)]  = 3, -- Polymorph
+		[GetSpellInfo(28272)] = 3, -- Polymorph pig
+		[GetSpellInfo(28271)] = 3, -- Polymorph turtle
+		[GetSpellInfo(61305)] = 3, -- Polymorph black cat
+		[GetSpellInfo(61025)] = 3, -- Polymorph serpent
+		[GetSpellInfo(51514)] = 3, -- Hex
+		[GetSpellInfo(710)] = 3, -- Banish
+		[GetSpellInfo(1499)] = 3, -- Freezing Trap Effect
+		[GetSpellInfo(60192)] = 3, -- Freezing Trap (from trap launcher)
 
 		-- Roots
-		[GetSpellInfo(339)] 	= 3, 	-- Entangling Roots
-		[GetSpellInfo(122)]		= 3,	-- Frost Nova
-		[GetSpellInfo(16979)] 	= 3, 	-- Feral Charge
-		[GetSpellInfo(13809)] 	= 1, 	-- Frost Trap
+		[GetSpellInfo(339)] = 3, -- Entangling Roots
+		[GetSpellInfo(122)] = 3, -- Frost Nova
+		[GetSpellInfo(16979)] = 3, -- Feral Charge
+		[GetSpellInfo(13809)] = 1, -- Frost Trap
 		[GetSpellInfo(113724)]  = 3, -- Ring of Frost
-		[GetSpellInfo(120)]	  = 1, -- Cone of Cold
+		[GetSpellInfo(120)]  = 1, -- Cone of Cold
 
 		-- Stuns and incapacitates
-		[GetSpellInfo(5211)] 	= 3, 	-- Bash
-		[GetSpellInfo(1833)] 	= 3,	-- Cheap Shot
-		[GetSpellInfo(408)] 	= 3, 	-- Kidney Shot
-		[GetSpellInfo(1776)]	= 3, 	-- Gouge
-		[GetSpellInfo(44572)]	= 3, 	-- Deep Freeze
-		[GetSpellInfo(19386)]	= 3, 	-- Wyvern Sting
+		[GetSpellInfo(5211)] = 3, -- Bash
+		[GetSpellInfo(1833)] = 3, -- Cheap Shot
+		[GetSpellInfo(408)] = 3, -- Kidney Shot
+		[GetSpellInfo(1776)] = 3, -- Gouge
+		[GetSpellInfo(44572)] = 3, -- Deep Freeze
+		[GetSpellInfo(19386)] = 3, -- Wyvern Sting
 		[GetSpellInfo(126246)]  = 3,    -- Lullaby
-		[GetSpellInfo(19503)] 	= 3, 	-- Scatter Shot
-		[GetSpellInfo(9005)]	= 3, 	-- Pounce
-		[GetSpellInfo(22570)]	= 3, 	-- Maim
-		[GetSpellInfo(853)]		= 3, 	-- Hammer of Justice
-		[GetSpellInfo(20066)] 	= 3, 	-- Repentance
-		[GetSpellInfo(46968)] 	= 3, 	-- Shockwave
-		--[GetSpellInfo(49203)] 	= 3,	-- Hungering Cold
-		[GetSpellInfo(47481)]	= 3,	-- Gnaw (dk pet stun)
+		[GetSpellInfo(19503)] = 3, -- Scatter Shot
+		[GetSpellInfo(9005)] = 3, -- Pounce
+		[GetSpellInfo(22570)] = 3, -- Maim
+		[GetSpellInfo(853)] = 3, -- Hammer of Justice
+		[GetSpellInfo(20066)] = 3, -- Repentance
+		[GetSpellInfo(46968)] = 3, -- Shockwave
+		[GetSpellInfo(47481)] = 3, -- Gnaw (dk pet stun)
 		[GetSpellInfo(90337)]  = 3, -- Bad Manner (monkey blind)
 		[GetSpellInfo(118905)] = 3, -- Static Charge - Capacitor Totem
 
 		-- Silences
-		[GetSpellInfo(55021)] 	= 1,	-- Improved Counterspell
-		[GetSpellInfo(15487)] 	= 1, 	-- Silence
-		[GetSpellInfo(34490)] 	= 1, 	-- Silencing Shot
-		--[GetSpellInfo(18425)]	= 1,	-- Improved Kick GONE
-		[GetSpellInfo(47476)]	= 1,	-- Strangulate
-		[GetSpellInfo(96231)]	= 1,  -- Rebuke					 unsure
-		--[GetSpellInfo(85388)]	= 1,  -- Throwdown GONE
-		[GetSpellInfo(80964)]	= 1,  -- Skull Bash
-		[GetSpellInfo(703)]	  = 1,  -- Garrote
+		[GetSpellInfo(55021)] = 1, -- Improved Counterspell
+		[GetSpellInfo(15487)] = 1, -- Silence
+		[GetSpellInfo(34490)] = 1, -- Silencing Shot
+		[GetSpellInfo(47476)] = 1, -- Strangulate
+		[GetSpellInfo(96231)] = 1, -- Rebuke unsure
+		[GetSpellInfo(80964)] = 1, -- Skull Bash
+		[GetSpellInfo(703)]  = 1, -- Garrote
 
 		-- Disarms
-		[GetSpellInfo(676)] 		= 1, 	-- Disarm
-		[GetSpellInfo(51722)] 	= 1,	-- Dismantle
+		[GetSpellInfo(676)] = 1, -- Disarm
+		[GetSpellInfo(51722)] = 1, -- Dismantle
 
 		-- Buffs
-		[GetSpellInfo(1022)] 	= 1,	-- Blessing of Protection
-		[GetSpellInfo(1044)] 	= 1, 	-- Blessing of Freedom
---		[GetSpellInfo(2825)] 	= 1, 	-- Bloodlust	old school shit
---		[GetSpellInfo(32182)] 	= 1, 	-- Heroism	  lets roll
-		[GetSpellInfo(33206)] 	= 1, 	-- Pain Suppression
-		[GetSpellInfo(29166)] 	= 1,	-- Innervate
-		--[GetSpellInfo(18708)]  	= 1,	-- Fel Domination GONE
-		[GetSpellInfo(54428)]	= 1,	-- Divine Plea
-		[GetSpellInfo(31821)]	= 1,	-- Aura mastery
-		[GetSpellInfo(118009)]  = 1, -- Desecrated Ground (DK lvl90 anti-CC)
+		[GetSpellInfo(1022)] = 1, -- Blessing of Protection
+		[GetSpellInfo(1044)] = 1, -- Blessing of Freedom
+		[GetSpellInfo(33206)] = 1, -- Pain Suppression
+		[GetSpellInfo(29166)] = 1, -- Innervate
+		[GetSpellInfo(54428)] = 1, -- Divine Plea
+		[GetSpellInfo(31821)] = 1, -- Aura mastery
+		[GetSpellInfo(118009)] = 1, -- Desecrated Ground (DK lvl90 anti-CC)
 		[GetSpellInfo(12292)] = 1, -- Death Wish
 		[GetSpellInfo(49016)] = 1, -- Unholy Frenzy
 
 		-- Turtling abilities
-		[GetSpellInfo(871)]		= 1,	-- Shield Wall
-		[GetSpellInfo(48707)]	= 1,	-- Anti-Magic Shell
-		[GetSpellInfo(31224)]	= 1,	-- Cloak of Shadows
-		[GetSpellInfo(19263)]	= 1,	-- Deterrence
-		[GetSpellInfo(76577)]	= 1, -- Smoke Bomb
-		[GetSpellInfo(74001)]	= 1, -- Combat Readiness
-		[GetSpellInfo(49039)]	= 1, -- Lichborn
-		[GetSpellInfo(47585)]	= 1, -- Dispersion
+		[GetSpellInfo(871)] = 1, -- Shield Wall
+		[GetSpellInfo(48707)] = 1, -- Anti-Magic Shell
+		[GetSpellInfo(31224)] = 1, -- Cloak of Shadows
+		[GetSpellInfo(19263)] = 1, -- Deterrence
+		[GetSpellInfo(76577)] = 1, -- Smoke Bomb
+		[GetSpellInfo(74001)] = 1, -- Combat Readiness
+		[GetSpellInfo(49039)] = 1, -- Lichborn
+		[GetSpellInfo(47585)] = 1, -- Dispersion
 
 		-- Immunities
-		[GetSpellInfo(34692)] 	= 1, 	-- The Beast Within
-		[GetSpellInfo(45438)] 	= 2, 	-- Ice Block
-		[GetSpellInfo(642)] 		= 2,	-- Divine Shield
+		[GetSpellInfo(34692)] = 1, -- The Beast Within
+		[GetSpellInfo(45438)] = 2, -- Ice Block
+		[GetSpellInfo(642)] = 2, -- Divine Shield
 	}, {
 		__index = function(t, index)
 			if (index ~= nil) then
