1d10fc7d54df4ed97d56465b609a2b1e945ed710
vendethiel <vendethiel@hotmail.fr>
2017-08-08 22:43:57 +0200

    CLASS_BUTTONS -> CLASS_ICON_TCOORDS

--- modules/unitbar.lua
+++ modules/unitbar.lua
@@ -62,6 +62,18 @@ function GladiusEx:NewUnitBarModule(name, defaults_arena, defaults_party)
 
 			self.frame[unit].statusbar:SetStatusBarColor(color.r, color.g, color.b, color.a or 1)
 			self.frame[unit].icon:SetTexture([[Interface\Glues\CharacterCreate\UI-CharacterCreate-Classes]])
+			local left, right, top, bottom = unpack(CLASS_ICON_TCOORDS[class])
+			if self.db[unit].IconCrop then
+				local n = 5
+				-- zoom class icon
+				left = left + (right - left) * (n / 64)
+				right = right - (right - left) * (n / 64)
+				top = top + (bottom - top) * (n / 64)
+				bottom = bottom - (bottom - top) * (n / 64)
+			end
+
+			self.frame[unit].icon:SetTexCoord(left, right, top, bottom)
+
 		end
 	end
 
