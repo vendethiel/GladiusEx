8c6a0d0b0f4b4c6fa738afb425c522b0dc726677
vendethiel <vendethiel@hotmail.fr>
2021-06-22 12:28:25 +0200

    add missing files

---
+++ Data-BCC.lua
@@ -0,0 +1,25 @@
+GladiusEx.Data = {}
+
+function GladiusEx.Data.DefaultAlertSpells()
+  return {}
+end
+
+function GladiusEx.Data.DefaultAuras()
+	return {}
+end
+
+function GladiusEx.Data.DefaultClassicon()
+  return {}
+end
+
+function GladiusEx.Data.DefaultCooldowns()
+	return {}
+end
+
+function GladiusEx.Data.InterruptModifiers()
+  return {}
+end
+
+function GladiusEx.Data.Interrupts()
+  return {}
+end
