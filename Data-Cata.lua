ebcad22b59b6903f415c91fd4fa0ed74c27ecde3
ManneN1 <ManneN1@users.noreply.github.com>
2024-03-31 00:11:33 +0100

    Added Skull Bash

--- Data-Cata.lua
+++ Data-Cata.lua
@@ -373,14 +373,15 @@ end
 
 function GladiusEx.Data.Interrupts()
     return {
-        [19675] = {duration = 4}, -- Feral Charge Effect (Druid)
-        [2139] = {duration = 8}, -- Counterspell (Mage)
-        [1766] = {duration = 5}, -- Kick (Rogue)
-        [6552] = {duration = 4}, -- Pummel (Warrior)
-        [72] = {duration = 6}, -- Shield Bash (Warrior)
-        [57994] = {duration = 2}, -- Wind Shear (Shaman)
-        [19244] = {duration = 5}, -- Spell Lock (Warlock
-        [47528] = {duration = 5} -- Mind Freeze (Deathknight)
+        [19675] = { duration = 4 }, -- Feral Charge Effect (Druid)
+        [2139]  = { duration = 8 }, -- Counterspell (Mage)
+        [1766]  = { duration = 5 }, -- Kick (Rogue)
+        [6552]  = { duration = 4 }, -- Pummel (Warrior)
+        [72]    = { duration = 6 }, -- Shield Bash (Warrior)
+        [57994] = { duration = 2 }, -- Wind Shear (Shaman)
+        [19244] = { duration = 5 }, -- Spell Lock (Warlock
+        [47528] = { duration = 5 }, -- Mind Freeze (Deathknight)
+        [93985] = { duration = 4 }, -- Skull Bash (Druid)
     }
 end
 
