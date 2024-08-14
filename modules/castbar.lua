eff320281190af1188fc988cb4abd51abd5aa2ae
ado21 <37584969+ado21@users.noreply.github.com>
2024-08-15 00:07:27 +0200

    call wrapper function

--- modules/castbar.lua
+++ modules/castbar.lua
@@ -546,7 +546,7 @@ function CastBar:Test(unit)
 	local f = self.frame[unit]
 
 	if GladiusEx.testing[unit].powerType == 0 then
-		local spell, displayName, icon, startTime, endTime, isTradeSkill, lineID, notInterruptible = L["Example Spell Name"], "", GetSpellTexture(1),
+		local spell, displayName, icon, startTime, endTime, isTradeSkill, lineID, notInterruptible = L["Example Spell Name"], "", GladiusEx:GetSpellTextureWrapper(1),
 			GetTime() * 1000 - 1000, GetTime() * 1000 + 1500, false, 0, false
 
 		f.spellName = spell
